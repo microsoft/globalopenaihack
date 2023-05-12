112a6035b1c3ce81897d03c1f0fc95fb12870400
Kyle Raymond <kyleraymond@microsoft.com>
2023-05-12 08:33:12 -0500

    initial checking

---
+++ SentimentAnalysis/AzureOpenAICSharrp/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using Azure;
+using Azure.AI.OpenAI;
+string endpoint = "https://your.azure.openai.endpoint/";
+string key = "api-key-found-in-azure-openai";
+
+
+OpenAIClient client = new(new Uri(endpoint), new AzureKeyCredential(key));
+
+var completionsOptions = new CompletionsOptions()
+{   Prompts = { @$"Classify the sentiment in this text.
+     ----
+     Text
+     I don't like Pizza.
+    ----
+   Is the text Positive, Neutral or Negative?"},
+ };
+
+
+
+Response<Completions> completionsResponse = client.GetCompletions("AzureOpenAI Studio deployment model name", completionsOptions);
+
+string completion = completionsResponse.Value.Choices[0].Text;
+Console.WriteLine(completion);
---
+++ SentimentAnalysis/OpenAICSharp/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Text;
+using Newtonsoft.Json;
+
+
+string apiUrl = "https://api.openai.com/v1/completions";
+string apiKey = "Your-OpenAI-API-Key";
+
+string statement = "I can't stand homework";
+
+var client = new HttpClient();
+var request = new HttpRequestMessage(HttpMethod.Post, apiUrl);
+request.Headers.Add("Authorization", $"Bearer {apiKey}");
+var body = new {
+    model = "text-curie-001", // Specify the OpenAI model you wish to use
+    prompt = $"Classify the sentiment in this text: {statement}",
+    temperature = 0,
+    max_tokens = 60,
+    top_p = 1,
+    frequency_penalty = 0,
+    presence_penalty = 0
+};
+var objAsJson = JsonConvert.SerializeObject(body);
+var content = new StringContent(objAsJson, Encoding.UTF8, "application/json");
+
+request.Content = content;
+var response = await client.SendAsync(request);
+response.EnsureSuccessStatusCode();
+
+string jsonResponse = await response.Content.ReadAsStringAsync();
+dynamic? result = JsonConvert.DeserializeObject(jsonResponse);
+Console.WriteLine(result!.choices[0].text);
