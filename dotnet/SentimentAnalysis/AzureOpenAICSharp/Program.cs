01bc9c22d6ff14bef035be4609f1c6d73da66666
Kyle Raymond <kyleraymond@microsoft.com>
2023-05-18 09:50:41 -0500

    removed comment

--- dotnet/SentimentAnalysis/AzureOpenAICSharp/Program.cs
+++ dotnet/SentimentAnalysis/AzureOpenAICSharp/Program.cs
@@ -3,8 +3,6 @@ using Azure;
 using Azure.AI.OpenAI;
 string endpoint = "https://your.azure.openai.endpoint/";
 string key = "api-key-found-in-azure-openai";
-
-//Curie is a fast and less expensive model that is good for sentiment analysis, summarization, and text generation.
 string deploymentName = "AzureOpenAI Studio deployment model name";
 
 OpenAIClient client = new(new Uri(endpoint), new AzureKeyCredential(key));
